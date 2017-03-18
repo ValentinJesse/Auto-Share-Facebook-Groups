d58c5b809fb3b70be8ab224d38fdfce8aac87da4
ValentinJesse <valentinjesse@gmx.com>
2017-03-18 21:11:33 +0100

    Better description

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ caption_text = soup =open('title.txt', 'r').read()
 
 auto = AutoShare()
 
-auto.auth(uname='', passd='')
+auto.auth(uname='InsertYourEmailAddress', passd='InsertYourPassword')
 auto.process(post_url=post_url, caption_text=caption_text)
 
 
